f696f3d7ebdfb8937d970b6639cdaa17f21db3e5
Stephen J. Butler <stephen.butler@gmail.com>
2018-08-26 03:44:30 -0500

    Remove pointless output

--- _outputs.tf
+++ _outputs.tf
@@ -6,10 +6,6 @@ output "website_bucket" {
     value = "${aws_s3_bucket.website.bucket}"
 }
 
-output "website_bucket_domain_name" {
-    value = "${aws_s3_bucket.website.bucket_domain_name}"
-}
-
 output "website_bucket_regional_domain_name" {
     value = "${aws_s3_bucket.website.bucket_regional_domain_name}"
 }
