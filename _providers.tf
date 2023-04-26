91a56be0b7bf2ba4b1233deabab64cbcd1992f1d
Stephen J. Butler <stephen.butler@gmail.com>
2023-04-26 15:52:52 -0500

    Update terraform

--- _providers.tf
+++ _providers.tf
@@ -1,13 +1,13 @@
 terraform {
-    required_version = "~> 1.3.5"
+    required_version = "~> 1.4.5"
     required_providers {
         aws = {
             source  = "hashicorp/aws"
-            version = "~> 4.42.0"
+            version = "~> 4.64.0"
         }
         random = {
             source  = "hashicorp/random"
-            version = "~> 3.4.3"
+            version = "~> 3.5.1"
         }
         time = {
             source  = "hashicorp/time"
